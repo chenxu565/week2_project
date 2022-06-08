df7eae81ca029b01fba382c9b2c293a3e166405c
chenxu565 <106840474+chenxu565@users.noreply.github.com>
2022-06-08 17:48:00 +0000

    wip3

--- src/index.js
+++ src/index.js
@@ -22,15 +22,41 @@ if (document.readyState !== "loading") {
 
 function initialzeCode() {
   const commentButton = document.getElementById("add-comment");
-
+  /*
   commentButton.addEventListener("click", function () {
     const commentList = document.getElementById("comments");
     const bigText = document.getElementById("big-text");
-    let comment = bigText.value;
+    //    let comment = bigText.value;
     let newParagraph = document.createElement("li");
-    newParagraph.innerHTML = comment;
+    //    newParagraph.innerHTML = comment;
+    newParagraph.appendChild(document.createTextNode(bigText.value));
     commentList.appendChild(newParagraph);
     bigText.value = "";
+  });
+*/
+  commentButton.addEventListener("click", function () {
+    const layout = document.getElementsByClassName("menu-layout");
+    const bigText = document.getElementById("big-text");
+    //    const value = document.querySelector('#rating').value;
+    //    const rating = document.querySelector(`#rating option[value=${value}]`).innerText;
+    //  const rating = document.querySelector("#rating").getSelected()[0].textContent;
+    const element = document.getElementById("rating");
+    const rating = element.options[element.selectedIndex].text;
+    let newParagraph = document.createElement("div");
+    newParagraph.classList.add("comment");
+    //  let commentRating = document.createTextNode(rating.value);
+    let commentRating = document.createElement("div");
+    commentRating.classList.add("comment-rating");
+    commentRating.innerHTML = rating;
+    //  commentRating.classList.add("comment-rating");
+    let commentText = document.createElement("div");
+    commentText.classList.add("comment-text");
+    commentText.innerHTML = bigText.value;
+    //  let commentText = document.createTextNode(bigText.value);
+    //  commentText.classList.add("comment-text");
+    newParagraph.appendChild(commentRating);
+    newParagraph.appendChild(commentText);
+    layout[0].appendChild(newParagraph);
   });
 
   const removeCommentButton = document.getElementById("remove-comments");
